679dd46fbecadcb55aabd9e66ba6b4b5eadd9951
yangone <yangonee@gmail.com>
2016-10-19 16:50:40 +0800

    创建项目

---
+++ learning_log/wsgi.py
@@ -0,0 +1,27 @@
+"""
+WSGI config for learning_log project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+import leancloud
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
+
+application = get_wsgi_application()
+
+#leancloud初始化
+APP_ID = os.environ['LEANCLOUD_APP_ID']
+APP_KEY = os.environ['LEANCLOUD_APP_KEY']
+MASTER_KEY = os.environ['LEANCLOUD_APP_MASTER_KEY']
+
+leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
+# 如果需要使用 master key 权限访问 LeanCLoud 服务，请将这里设置为 True
+leancloud.use_master_key(False)
