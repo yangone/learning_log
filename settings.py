1ed510976ebfc8489fbe3812b9b4d031048765cb
yangone <yangonee@gmail.com>
2016-10-21 14:54:14 +0800

    edit settings middleware

--- settings.py
+++ settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'leancloud.engine.leanengine'
 ]
 
 ROOT_URLCONF = 'urls'
